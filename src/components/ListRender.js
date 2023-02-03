43bb5e83e8adafeef812fec4aa902a6a03f28ff6
Mariana Faustino <marifaustino@gmail.com>
2023-02-03 16:56:31 -0300

    chaves key

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -5,13 +5,30 @@ const ListRender = ()=> {
 
     const [list] = useState(['Pedro', 'Mateus', 'Lucas'])
 
+    const [users] = useState([
+      {id: 1, nome: 'joão', idade: 28},
+      {id: 25487852, nome: 'marly', idade: 52},
+      {id: 5478515, nome: 'mariana', idade: 45}
+    ])
+
   return (
+    <div>
     <div>
         <ul>
             {list.map((item, i)=>(
                 <li key={i}>{item}</li>
             ))}
         </ul>
+    </div>
+
+    <div>
+      <ul>
+          {users.map((user)=>(
+            <li key={user.id}> {user.nome} - {user.idade}</li>
+          ))}
+        </ul>
+    </div>
+
     </div>
   )
 }
